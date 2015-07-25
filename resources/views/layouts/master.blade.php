5c1eb84ebc45c45c177ebb906dc45c0aa73cc01f
fntzllcpeter <peter@fantagrade.com>
2015-07-25 08:09:55 -0700

    Added jQuery and Bootstrap

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fantagrade - @yield('title')</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css" type="text/css" media="all" />
+    <script type="text/javascript" src="assets/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+
+    <script type="text/javascript" src="assets/bootstrap.min.js"></script>
+</body>
+</html>
